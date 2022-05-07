63bc74c44cbeb4e1c894f1fb59da51b990b63a61
Anannya27 <104672818+Anannya27@users.noreply.github.com>
2022-05-07 13:31:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,29 @@ function start()
 recognition.onresult = function(event) {
     console.log(event);
     var Content = event.results[0][0].transcript;
-    console.log(Content);
+    
     document.getElementById("textbox").innerHTML = Content;
+
+    console.log(Content);
+    speak();
 }
+ 
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_formate : 'png' ,
+    png_quality:90
+});
+camera = document.getElementById("camera")
